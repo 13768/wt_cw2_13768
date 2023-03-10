66dbf2bc0c7cc8522e0c91eac8029dac1123843a
13768 <wiut13768@gmail.com>
2023-03-10 10:07:23 +0500

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'pug') // setting pug as the default view engine
+//localhost:8000
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.listen(PORT, (err) => {
+    if (err) throw err
+
+    console.log(`This app is running in port ${PORT}`)
+})
