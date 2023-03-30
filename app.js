c5e9f86dc032177cdb62d5206c3b9021113b637f
13768 <wiut13768@gmail.com>
2023-03-30 23:27:56 +0500

    fixed minor bugs and made small updates

--- app.js
+++ app.js
@@ -13,26 +13,80 @@ app.use('/static', express.static('public')) //can also be named assets
 app.use(express.urlencoded({ extended: false }))
 //localhost:8000
 app.get('/', (req, res) => {
-    fileSystem.readFile('./data/reviews.json', (err, data) => {
-        if (err) throw err
+    let q = req.query.added
 
-        const reviews = JSON.parse(data)
-
-        res.render('home', { reviews: reviews })
-    })
-    res.render('home')
+    if (added)
+    {
+        fileSystem.readFile('./data/reviews.json', (err, data) => {
+            if (err) throw err
+    
+            let reviews = JSON.parse(data)
+    
+            res.render('home', { reviews: reviews })
+        })
+    }
+    else
+    {
+        res.render('home', {})
+    }
+      
 })
 
+//TODO: work on add! 31.03.2023 Show the list of available books and show the chosen book
+app.get('/add', (req, res) => {
+    res.render('add', {})
+})
 
+app.post('/add', (req, res) => {
+    let formData = req.body
+    
+    if (formData.description.trim() == '')
+    {
+        res.render('add', { error: true })
+    }
+    else
+    {
+        let review = {
+            name: data.name,
+            rating: data.rating,
+            description: data.description
+        }
+    
+        let reviews = getAll('reviews')
+    
+        reviews.push(review)
+        writeAll('reviews', reviews)
+        res.redirect('/?added=true')
+        //The old version. Check the above, might be better
+        fileSystem.readFile('./data/reviews.json', (err, data) => {
+            if (err) throw err
+
+            //JSON.parse creates a javascript object from the string data in the json file 
+            const reviews = JSON.parse(data)
+            //We need to show the object(s) with the title or author that is similar to the value typed in the search
+            // field and if there are none, no items should be displayed
+            
+            //we are not creating any review object. This is just filtering so far.
+        })
+    }
+})
+
+function getAll(filename) {
+    return JSON.parse(fs.readFileSync(filename))
+}
+function writeAll(filename, data) {
+    return JSON.stringify(fs.writeFileSync(filename, data))
+}
 app.post('/search', (req, res) => {
     const formData = req.body
 
     if (formData.search.trim() == '')
     {
-        res.render('home', { error: true })
+        res.render('search', { error: true })
     }
     else
     {
+        //TODO: filter out the available books depending on the typed out text. Should we use javascript for front?
         //read is similar to establishing connection to the database
         fileSystem.readFile('./data/reviews.json', (err, data) => {
             if (err) throw err
